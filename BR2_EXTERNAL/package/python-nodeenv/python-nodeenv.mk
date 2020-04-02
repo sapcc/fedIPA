682e4994867bacf4689cfc6019f3ca491ece1a83
Silvio Knizek <knizek@b1-systems.de>
2020-04-02 12:00:27 +0200

    python-nodedev: set proper upstream source files

--- BR2_EXTERNAL/package/python-nodeenv/python-nodeenv.mk
+++ BR2_EXTERNAL/package/python-nodeenv/python-nodeenv.mk
@@ -6,8 +6,8 @@
 ################################################################################
 
 PYTHON_NODEENV_VERSION = 1.3.5
-PYTHON_NODEENV_SOURCE = Unknown
-PYTHON_NODEENV_SITE = Unknown
+PYTHON_NODEENV_SOURCE = 1.3.5.tar.gz
+PYTHON_NODEENV_SITE = https://github.com/ekalinin/nodeenv/archive/
 PYTHON_NODEENV_LICENSE = BSD-2-Clause
 PYTHON_NODEENV_LICENSE_FILES = LICENSE
 PYTHON_NODEENV_SETUP_TYPE = setuptools
