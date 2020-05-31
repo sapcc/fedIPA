c0001d6052df4752752392922670208ba442144c
Silvio Knizek <silvio.knizek@sap.com>
2020-05-31 12:39:24 +0000

    add files from dbus-common

--- mkosi.extra/usr/lib/dracut/modules.d/08dbus-broker/module-setup.sh
+++ mkosi.extra/usr/lib/dracut/modules.d/08dbus-broker/module-setup.sh
@@ -14,10 +14,29 @@ function depends() {
 }
 
 function install() {
+    # files from rpm:dbus-common
     inst_multiple dbus-broker dbus-broker-launch
     inst /usr/lib/systemd/catalog/dbus-broker-launch.catalog
     inst /usr/lib/systemd/catalog/dbus-broker.catalog
     inst /usr/lib/systemd/system/dbus-broker.service
     inst /usr/lib/systemd/user/dbus-broker.service
+    # files from rpm:dbus-common
+    inst /etc/dbus-1
+    inst /etc/dbus-1/session.conf
+    inst /etc/dbus-1/session.d
+    inst /etc/dbus-1/system.conf
+    inst /etc/dbus-1/system.d
+    inst /usr/lib/systemd/system/dbus.socket
+    inst /usr/lib/systemd/user/dbus.socket
+    inst /usr/lib/sysusers.d/dbus.conf
+    inst /usr/share/dbus-1
+    inst /usr/share/dbus-1/interfaces
+    inst /usr/share/dbus-1/services
+    inst /usr/share/dbus-1/session.conf
+    inst /usr/share/dbus-1/session.d
+    inst /usr/share/dbus-1/system-services
+    inst /usr/share/dbus-1/system.conf
+    inst /usr/share/dbus-1/system.d
+    systemctl -q --root "$initdir" enable dbus.socket
     systemctl -q --root "$initdir" enable dbus-broker.service
 }
